7eef50c9a7b05ad614d088862496ea91486d2df8
fyperia <dysfunctionalmusician@gmail.com>
2024-07-11 22:55:11 -0400

    - Added a header link to the index page. - Changed the url strings for class and skill to plural (from .../class/... to .../classes/...)

--- anoch_db/urls.py
+++ anoch_db/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'db'
 urlpatterns = [
     # .../db/
     path("", views.index, name="index"),
-    # .../db/skill/1
-    path("skill/<int:skill_id>/", views.skill, name="skill"),
-    # .../db/class/1
-    path("class/<int:class_id>/", views.character_class, name="class"),
+    # .../db/skills/1
+    path("skills/<int:skill_id>/", views.skill, name="skills"),
+    # .../db/classes/1
+    path("classes/<int:class_id>/", views.character_class, name="classes"),
 ]
